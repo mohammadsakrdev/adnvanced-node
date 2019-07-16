867943f3955d99e40c7269b665d3d0b546728779
Mohammad Sakr <mohamed.sakr@digis2.com>
2019-07-16 16:08:41 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const express = require('express');
-const crypto = require('crypto');
-const worker = require('worker_threads');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./models/Blog');
+require('./services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {
+    useMongoClient: true
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('hi there');
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
     })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
+
+if (['production'].includes(process.env.NODE_ENV)) {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve('client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Listening on port`, PORT);
 });
-
-app.get('/fast', (req, res) => {
-    res.send('hi fast there');
-});
-
-
-app.listen(3000);
